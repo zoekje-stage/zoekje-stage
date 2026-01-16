9f0fda54114f6a822dc74100609cb733ef1f2ecc
vasie <59412971+vasie1337@users.noreply.github.com>
2026-01-16 12:02:50 +0100

    add proper favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,14 +24,8 @@ export const metadata: Metadata = {
   creator: 'easystage.nl',
   publisher: 'easystage.nl',
   icons: {
-    icon: [
-      { url: '/favicon.ico', sizes: '32x32' },
-      { url: '/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
-      { url: '/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
-    ],
-    apple: [
-      { url: '/apple-touch-icon.png', sizes: '180x180', type: 'image/png' },
-    ],
+    icon: '/favicon.ico',
+    apple: '/apple-touch-icon.png',
   },
   formatDetection: {
     email: false,
